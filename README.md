229df9f5eecdb748cf972da3d2563f07bfafdf60
AquibPy <mohdaquib31@gmail.com>
2020-08-23 17:06:27 +0530

    modify1

---
+++ README.md
@@ -0,0 +1,5 @@
+# HaSa App
+![Python 3.8](https://img.shields.io/badge/Python-3.8-brightgreen.svg) ![NLTK](https://img.shields.io/badge/Library-NLTK-orange.svg)
+
+
+• Please do ⭐ the repository, if it helped you in anyway.
---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template,request
+import pickle
+
+classifier = pickle.load(open('model.pkl','rb'))
+cv = pickle.load(open('cv.pkl','rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_predict = classifier.predict(vect)
+        return render_template('result.html',prediction=my_predict)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
